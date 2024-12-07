7a461dc8d5f70c34e5bbd4350a96ac9325a324c4
Ryan Barry <ryan@nuclearice.com>
2024-12-07 15:33:29 -0800

    parsing stats html works (at least on the example)

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,39 @@
 use crate::{AocStats, DailyStats};
 
 pub fn parse_body(text: &str) -> AocStats {
+    println!("parsing {} chars", text.chars().count());
     let mut result: AocStats = [DailyStats {
         both: 0,
         firstonly: 0,
     }; 25];
-    // for i in 0..result.len() {
-    //     result[i].both = 0;
-    //     result[i].firstonly = 0;
-    // }
+    let dom = match tl::parse(text, tl::ParserOptions::new().track_classes()) {
+        Err(err) => panic!("got error parsing body: {err}"),
+        Ok(dom) => dom,
+    };
+    let parser = dom.parser();
+    let stats_both = dom
+        .query_selector(".stats-both")
+        .expect("failed to use query selector for .stats-both");
+    let stats_both: Vec<usize> = stats_both
+        .filter_map(|n| n.get(parser))
+        .map(|el| el.inner_html(parser).trim_start().to_string())
+        .filter_map(|t| t.parse().ok())
+        .collect();
+    let stats_firstonly = dom
+        .query_selector(".stats-firstonly")
+        .expect("failed to use query selector for .stats-firstonly");
+    let stats_firstonly: Vec<usize> = stats_firstonly
+        .filter_map(|n| n.get(parser))
+        .map(|el| el.inner_html(parser).trim_start().to_string())
+        .filter_map(|t| t.parse().ok())
+        .collect();
+    assert_eq!(stats_both.len(), 25, "wrong number of elements found matching both querysel");
+    assert_eq!(stats_firstonly.len(), 25, "wrong number of elements found matching firstonly querysel");
+    for (idx, (statboth, statfirstonly)) in stats_both.iter().rev().zip(stats_firstonly.iter().rev()).enumerate() {
+        println!("parsing day {idx}");
+        result[idx].both = *statboth;
+        result[idx].firstonly = *statfirstonly;
+    }
     result
 }
 
@@ -116,7 +141,8 @@ fn test_parse_body() {
             firstonly: 0,
         },
     ];
-    let stats = parse_body(include_str!("stats_body.in"));
+    let example_body = include_str!("stats_body.in");
+    let stats = parse_body(example_body);
     assert_eq!(
         stats.len(),
         expected.len(),
@@ -124,7 +150,10 @@ fn test_parse_body() {
     );
     for i in 0..stats.len() {
         print!("checking day {i}: ");
-        assert_eq!(stats[i], expected[i], "results on day {i} differ (left->got, right->expected)");
+        assert_eq!(
+            stats[i], expected[i],
+            "results on day {i} differ (left->got, right->expected)"
+        );
         println!("good");
     }
 }
